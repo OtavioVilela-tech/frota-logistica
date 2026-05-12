0e8af76b04713b24af83758bcf7615dfd19fa028
OtavioVilela-tech <otavioviba@gmail.com>
2026-05-12 20:07:42 -0300

    Update Viagem.php
    mudança na classe viagem

---
+++ Viagem.php
@@ -0,0 +1,36 @@
+<?php
+
+class Viagem {
+
+    private string $destino;
+    private float $distancia;
+    private Motorista $motorista;
+    private Veiculo $veiculo;
+
+    public function __construct(
+        string $destino,
+        float $distancia,
+        Motorista $motorista,
+        Veiculo $veiculo
+    ) {
+
+        $this->destino = $destino;
+        $this->distancia = $distancia;
+        $this->motorista = $motorista;
+        $this->veiculo = $veiculo;
+    }
+
+    public function iniciarViagem(): void {
+
+        if($this->motorista->getValidadeCnh() < 2026) {
+
+            echo "CNH vencida.<br>";
+            return;
+        }
+
+        echo "Viagem iniciada.<br>";
+
+        $this->veiculo->viajar($this->distancia);
+    }
+
+?>
