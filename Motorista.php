567ecec03df5f92d1cda224787144787c4e867b8
OtavioVilela-tech <otavioviba@gmail.com>
2026-05-12 19:41:33 -0300

    Update Motorista.php
    classe motorista realizada

--- Motorista.php
+++ Motorista.php
@@ -3,6 +3,34 @@
 class Motorista
 {
     private $nome;
-    private $validadecnh;
+    private $cpf;
+    private $cnh;
+    private $validadeCnh;
+
+    public function __construct($nome, $cpf, $cnh, $validadeCnh) 
+    {
+        $this->setNome($nome);
+        $this->cpf = $cpf;
+        $this->cnh = $cnh;
+        $this->validadeCnh = $validadeCnh;
+    }
     
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getValidadeCnh() {
+        return $this->validadeCnh;
+    }
+
+    
+    public function setNome($nome) {
+
+        if(empty($nome)) {
+            echo "Nome inválido!<br>";
+        } else {
+            $this->nome = $nome;
+        }
+    }
 }
+?>
